9a8986c005132a9e491da545a91a7991cde07a3c
Robin Hamill <hamillrobin@gmail.com>
2018-08-23 15:50:54 -0400

    Add basic form and Mailchimp script

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,30 @@
 <template>
-  <main id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <h1>Vue JS Workshop</h1>
-    <h2>Coming soon!</h2>
-  </main>
+  <div class="wrapper">
+    <main id="app">
+      <img alt="Vue logo" src="./assets/logo.png">
+      <h1>Vue JS Workshop</h1>
+      <h2>Coming soon!</h2>
+      <email-signup-form />
+    </main>
+  </div>
 </template>
 
 <script>
+import EmailSignupForm from './components/EmailSignupForm.vue';
 
 export default {
   name: 'app',
   components: {
+    EmailSignupForm,
   },
 };
 </script>
 <style lang="stylus">
+.wrapper
+  max-width 1000px
+  width 100%
+  margin 0 auto
+
 #app
   font-family 'Avenir', Helvetica, Arial, sans-serif
   -webkit-font-smoothing antialiased
---
+++ src/components/EmailSignupForm.vue
@@ -0,0 +1,16 @@
+<template>
+  <form>
+    <label for="email">Email</label>
+    <input type="email" name="email" id="email">
+    <input type="submit" value="Sign up!">
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'EmailSignupForm',
+};
+</script>
+
+<style scoped lang="stylus">
+</style>
