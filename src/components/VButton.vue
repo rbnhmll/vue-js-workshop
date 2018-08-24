d93b43154cec23d6b06d27fa1690bae092ea0583
Robin Hamill <hamillrobin@gmail.com>
2018-08-24 17:21:43 -0400

    Create VButton comp

---
+++ src/components/VButton.vue
@@ -0,0 +1,17 @@
+<template>
+  <a class="btn" :href="url">
+    <slot></slot>
+  </a>
+</template>
+
+<script>
+export default {
+  name: 'VButton',
+  props: {
+    url: String,
+  },
+};
+</script>
+
+<style scoped lang="stylus">
+</style>
