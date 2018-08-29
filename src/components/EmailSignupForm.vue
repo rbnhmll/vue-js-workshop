73328b3df40e7b505fef889168c758dd510a9e5e
Robin Hamill <hamillrobin@gmail.com>
2018-08-29 16:37:06 -0400

    Remove console.log

--- src/components/EmailSignupForm.vue
+++ src/components/EmailSignupForm.vue
@@ -65,7 +65,6 @@ export default {
       const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
       const result = re.test(email);
       this.validEmail = result;
-      console.log(this.validEmail, result);
     },
   },
 };
