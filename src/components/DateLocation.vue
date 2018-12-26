f1abbb7dbbf1fe60215021794bb3d700861e743d
Robin Hamill <hamillrobin@gmail.com>
2018-12-26 14:19:18 -0700

    Update date

--- src/components/DateLocation.vue
+++ src/components/DateLocation.vue
@@ -2,28 +2,29 @@
   <div>
     <template v-if="!location.TBD">
       <h2 class="h4 heading">
-        At <a :href="location.venue.mapLink" target="_blank">{{ location.venue.name }}</a>
+        At
+        <a :href="location.venue.mapLink" target="_blank">{{ location.venue.name }}</a>
         &bull; {{ location.date }}
       </h2>
       <p>{{ location.time }}</p>
     </template>
     <template v-else>
-      <h2 class="h4 heading">Toronto &bull; January 2019</h2>
+      <h2 class="h4 heading">Toronto &bull; TBA</h2>
     </template>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'DateLocation',
+  name: "DateLocation",
   props: {
-    eventDetails: Object,
+    eventDetails: Object
   },
   computed: {
     location() {
       return this.eventDetails.event.location;
-    },
-  },
+    }
+  }
 };
 </script>
 
