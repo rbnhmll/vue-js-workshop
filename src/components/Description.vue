51f9cd1ef59fc9a71a7933996605e9c024a441fa
Robin Hamill <hamillrobin@gmail.com>
2020-03-06 17:23:44 -0500

    Add noopener noreferrer, fix JS spelling

--- src/components/Description.vue
+++ src/components/Description.vue
@@ -2,7 +2,7 @@
   <section>
     <h2 class="standout">In this workshop, you will learn the key concepts and techniques
       to get started using
-      <a href="https://vuejs.org/" target="_blank">Vue.js</a>.</h2>
+      <a href="https://vuejs.org/" target="_blank" rel="noopener noreferrer">Vue.js</a>.</h2>
 
     <h3 class="h4">Together you will build a fully functional web application using the Vue CDN,
       as well as the new Vue CLI 3!</h3>
@@ -12,8 +12,8 @@
       to help make our code manageable and maintainable.</p>
 
     <p>One of these libraries is Vue.js:
-      the most starred open source project on <a href="https://github.com/vuejs/vue">Github</a> with over 112k stars,
-      and one of the fastest growing javascript libraries in popularity.</p>
+      the most starred open source project on <a href="https://github.com/vuejs/vue" target="_blank" rel="noopener noreferrer">Github</a> with over 159k stars,
+      and one of the fastest growing JavaScript libraries in popularity.</p>
   </section>
 </template>
 
